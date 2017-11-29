e1a633234630ee57412dbf07fd0d1984348ac99f
Dalia Galván <idalikg@gmail.com>
2017-11-28 22:06:05 -0600

    Instalación de Mongoose Instalación del módulo 'mongoose' de NPM que servirá como driver para establecer la conexión entre la API-REST y la Base de Datos en MongoDB. Asimismo en el archivo 'app.js' se configuró la conexión empleando el método '.conecti on' de mongoose y se replanteó la ejecución del servidor.

--- app.js
+++ app.js
@@ -35,7 +35,13 @@ app.delete('/api/contact/:contactID', (req, res) => {
     res.status(200).send({ message : `${req.params.contactID} DELECTED`});
 });
 
-// Server
-app.listen(port, () => {
-    console.log(`${app.get('nameAPI')} running on port ${port}`);
+// Mongoose & Server
+mongoose.connection.openUri('mongodb://localhost:27017/diary', (err, res) => {
+    if(err) throw err
+    console.log('Conexión SATISFACTORIA a la Base de Datos en MongoDB');
+
+    // Server running
+    app.listen(port, () => {
+        console.log(`${app.get('nameAPI')} running on port ${port}`);
+    });
 });
