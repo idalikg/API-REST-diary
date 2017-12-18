c74af9a219a5301e4427f01b719ee7ed17cf4bb0
Dalia Galván <idalikg@gmail.com>
2017-12-17 23:21:05 -0600

    Controlador de Rutas Se creó la carpeta 'api' donde a la vez se tiene la sub-carpeta 'routes' en la cual se creó el archivo 'contacts.js' con la finalidad de almacenar en este el bloque de código correspondiente a las rutas previamente declaradas en el archivo 'app.js' y del cual fueron eliminadas. No obstante, en este último archivo es requirido el archivo 'contacts.js' para que a través de un middleware se tenga acceso a dichas rutas y permitir así que la aplicación en express funcione.

---
+++ api/routes/contacts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express.Router();
+
+const Contact = require('../../model/contact.js');
+
+app.get('/contact', (req, res) => {
+    // res.status(200).send('Contacts List');
+
+    Contact.find({}, (err, contactos) => {
+        if (err) return res.status(500).send({ message: 'Hubo un error al ralizar la petición' })
+
+        if (!contactos) return res.status(404).send({ message: 'Aún no hay contactos' })
+
+        res.status(200).send({ contactos });
+    });
+});
+
+app.get('/contact/:contactID', (req, res) => {
+    res.status(200).send({ message: `Contact ${req.params.contactID} LOCATED` });
+});
+
+app.post('/contact', (req, res) => {
+    res.status(200).send({ message: 'Contact REGISTERED' });
+});
+
+app.put('/contact/:contactID', (req, res) => {
+    res.status(200).send({ message: `${req.params.contactID} UPDATED` });
+});
+
+app.delete('/contact/:contactID', (req, res) => {
+    res.status(200).send({ message: `${req.params.contactID} DELECTED` });
+});
+
+module.exports = app;
--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
-const Contact = require('./model/contact.js');
-
 const app = express();
+
+const contactRoutes = require('./api/routes/contacts');
 
 // Settings
 app.set('nameAPI','API-REST_diary');
@@ -16,32 +15,6 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
 // Routes
-app.get('/api/contact', (req, res) => {
-    // res.status(200).send('Contacts List');
-
-    Contact.find({}, (err, contactos) => {
-        if(err) return res.status(500).send({ message : 'Hubo un error al ralizar la petición'})
-
-        if(!contactos) return res.status(404).send({ message : 'Aún no hay contactos' })
-
-        res.status(200).send({ contactos });
-    });
-});
-
-app.get('/api/contact/:contactID', (req, res) => {
-    res.status(200).send({ message : `Contact ${req.params.contactID} LOCATED`});
-});
-
-app.post('/api/contact', (req, res) => {
-    res.status(200).send({ message :   'Contact REGISTERED'});
-});
-
-app.put('/api/contact/:contactID', (req, res) => {
-    res.status(200).send({ message : `${req.params.contactID} UPDATED` });
-});
-
-app.delete('/api/contact/:contactID', (req, res) => {
-    res.status(200).send({ message : `${req.params.contactID} DELECTED`});
-});
+app.use('/api', contactRoutes);
 
 module.exports = app; 
